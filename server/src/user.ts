633ea72fe48c53e45c455b531e53c2cda1f09ae4
Dawniqueca Steele <dawniquecas@gmail.com>
2026-07-10 01:11:40 -0400

    Client/Server Merged + Register Service added

--- server/src/user.ts
+++
@@ -1,8 +0,0 @@
-import * as mongodb from "mongodb";
-
-export interface User {
-    name: string;
-    username: string;
-    password: string;
-    id?: mongodb.ObjectId;
-}
---
+++ src/app/auth/register/register.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,ReactiveFormsModule, Validators} from '@angular/forms'
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+  registerForm: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.registerForm=this.fb.group({
+      name:["", Validators.required],
+      email:["", [Validators.required, Validators.email]],
+      password:["", [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  onSubmit(){
+    
+  }
+}
