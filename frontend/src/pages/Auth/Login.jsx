cd18977820111780f326e4dfa6985aec81475f96
Dawniqueca Steele <dawniquecas@gmail.com>
2026-07-19 01:26:56 -0400

    backend structure + db connection

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -2,7 +2,15 @@ import React from "react"
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div>
+            <div className="flex items-center justify-center h-[100vh] p-20">
+                <div className="">
+                    <h3 className="">Cococoins</h3>
+                    <p className="">Log in</p>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
